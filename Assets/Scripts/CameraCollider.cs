ccebe6e9f341f26f0cc58eaa9d42de3ce0f05f80
Jeremy <jeremymechen1997@gmail.com>
2017-09-11 02:43:26 +1200

    Implemented Camera Tracking

---
+++ Assets/Scripts/CameraCollider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollider : MonoBehaviour {
+
+    CameraFollower cameraScript;
+
+	// Use this for initialization
+	void Start () {
+        cameraScript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollower>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+
+        if (coll.gameObject.tag == "Protag")
+        {
+            cameraScript.setFollowing(false);
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+
+        if (coll.gameObject.tag == "Protag")
+        {
+            cameraScript.setFollowing(true);
+        }
+
+    }
+}
---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+
+    public Transform target;
+
+    public float zPos = -10f;
+
+    public bool following = true;
+
+    float offsetX = 0f;
+    float offsetY = 0f;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("Protag").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (following)
+        {
+
+            this.transform.position = new Vector3(target.position.x - offsetX, target.position.y - offsetY, zPos);
+
+        }
+        else
+        {
+            this.offsetX = target.transform.position.x - this.transform.position.x;
+            this.offsetY = target.transform.position.y - this.transform.position.y;
+        }
+
+
+	}
+
+    public void setFollowing(bool state)
+    {
+        this.following = state;
+    }
+}
