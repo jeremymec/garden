ba00c5ab9ed9ecf34f249113b417e64fd44e6e14
Jeremy <jeremymechen1997@gmail.com>
2017-09-12 22:10:49 +1200

    Basic action system

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action : MonoBehaviour {
+
+    public static KeyCode key = KeyCode.F;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(key))
+        {
+            print("Player Interact");
+        }
+	}
+}
---
+++ Assets/Scripts/StateController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateController : MonoBehaviour {
+
+    public enum STATE
+    {
+        Normal,
+        Dialog
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void changeState(STATE state)
+    {
+
+        switch (state)
+        {
+            case STATE.Dialog:
+                PlayerMovement playerMovement = GetComponent<PlayerMovement>();
+                playerMovement.setFrozen(true);
+                break;
+        }
+
+    }
+
+
+
+}
